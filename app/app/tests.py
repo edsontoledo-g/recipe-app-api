7dc06247e4ca2bb76a75404d3143bf5879ed50b1
Edson Toledo <edson.toledo.g@gmail.com>
2023-12-09 14:34:02 -0600

    Add PostgreSQL dependencies

---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calculator
+
+class CalculatorTest(SimpleTestCase):
+    """ Test the calculator module """
+    def test_add_numbers(self):
+        """ Test adding two numbers """
+        result = calculator.add(1, 2)
+        self.assertEqual(result, 3)
+    
+    def test_subtract_numbers(self):
+        """ Test subtracting two numbers """
+        result = calculator.subtract(2, 1)
+        self.assertEqual(result, 1)
