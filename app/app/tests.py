af17f99c34a80ba84fc5bc19a1a909fef194eefe
Edson Toledo <edson.toledo.g@gmail.com>
2023-12-11 22:13:47 -0600

    Setup database

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,13 +5,15 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calculator
 
+
 class CalculatorTest(SimpleTestCase):
     """ Test the calculator module """
+
     def test_add_numbers(self):
         """ Test adding two numbers """
         result = calculator.add(1, 2)
         self.assertEqual(result, 3)
-    
+
     def test_subtract_numbers(self):
         """ Test subtracting two numbers """
         result = calculator.subtract(2, 1)
